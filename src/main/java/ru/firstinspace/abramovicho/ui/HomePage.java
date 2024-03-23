5af9a8985f0fef4bc0bd4aed0827729e59fbfee7
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-23 21:32:00 +0300

    Add selectors + methods

--- src/main/java/ru/firstinspace/abramovicho/ui/HomePage.java
+++ src/main/java/ru/firstinspace/abramovicho/ui/HomePage.java
@@ -1,7 +1,13 @@
 package ru.firstinspace.abramovicho.ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
@@ -10,15 +16,31 @@ public class HomePage {
         this.driver = Driver.getDriver();
     }
 
+    private By subscriptionDialog = By.xpath("//div[@class='subscription']");
+    private By closeSubscription = By.cssSelector("button.subscription__close");
     private By searchField = By.cssSelector("button.header__option_search");
     private By favoritesButton = By.cssSelector(".header__wrapper.mobile-none [href='/favorites/']");
-    private By cartButton = By.cssSelector("");
-    private By cartCounter = By.cssSelector("//div[contains(@class,'mobile-none')]//span[@class='header__option-count']");
-    private By profileButton = By.cssSelector("");
+    private By cartButton = By.xpath("(//div[contains(@class,'mobile-none')]//button)[2]");
+    private By cartCounter = By.xpath("//div[contains(@class,'mobile-none')]//span[@class='header__option-count']");
+    private By profileButton = By.xpath("(//div[contains(@class,'mobile-none')]//button)[3]");
+    private By catalogItem = By.cssSelector(".catalog__card");
 
     public LoginPage openLoginPage() {
         driver.findElement(profileButton).click();
         return new LoginPage();
+    }
+
+    public void inputTextInSearchField(String text) {
+        WebElement search = driver.findElement(searchField);
+        search.sendKeys(text);
+        search.sendKeys(Keys.ENTER);
+    }
+
+    public void closeSubscriptionDialog(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(subscriptionDialog));
+        driver.findElement(closeSubscription).click();
+
     }
 
 }
