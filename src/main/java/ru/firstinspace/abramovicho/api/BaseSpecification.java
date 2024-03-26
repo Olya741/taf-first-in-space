8ab552800d3c767d13b9266ad53218b0927d2810
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-26 23:24:11 +0300

    Add BaseSpecification.java

---
+++ src/main/java/ru/firstinspace/abramovicho/api/BaseSpecification.java
@@ -0,0 +1,13 @@
+package ru.firstinspace.abramovicho.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseSpecification {
+    final static String BASE_URI = "https://www.first-in-space.ru/api";
+
+    static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri(BASE_URI)
+            .setContentType("application/json")
+            .build();
+}
