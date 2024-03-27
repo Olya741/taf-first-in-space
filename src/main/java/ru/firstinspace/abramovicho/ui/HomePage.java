6a66966e505b8999eca000db165b9e469dcd39ac
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-28 01:04:12 +0300

    Add method openSection()

--- src/main/java/ru/firstinspace/abramovicho/ui/HomePage.java
+++ src/main/java/ru/firstinspace/abramovicho/ui/HomePage.java
@@ -1,13 +1,12 @@
 package ru.firstinspace.abramovicho.ui;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePage {
     private WebDriver driver;
@@ -23,7 +22,7 @@ public class HomePage {
     private By cartButton = By.xpath("(//div[contains(@class,'mobile-none')]//button)[2]");
     private By cartCounter = By.xpath("//div[contains(@class,'mobile-none')]//span[@class='header__option-count']");
     private By profileButton = By.xpath("(//div[contains(@class,'mobile-none')]//button)[3]");
-    private By menuItem = By.cssSelector(".header__wrapper.mobile-none .nav__item");
+    private By section = By.xpath("//div[contains(@class, 'mobile-none')]//li[@class='nav__item']");
     private By catalogItem = By.cssSelector(".catalog__card");
 
     public LoginPage openLoginPage() {
@@ -37,10 +36,25 @@ public class HomePage {
         search.sendKeys(Keys.ENTER);
     }
 
-    public void closeSubscriptionDialog(){
+    public void closeSubscriptionDialog() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         wait.until(ExpectedConditions.visibilityOfElementLocated(subscriptionDialog));
         driver.findElement(closeSubscription).click();
     }
 
+    private List<WebElement> getListOfMenuItems() {
+        return driver.findElements(section);
+    }
+
+    private WebElement filterList(Menu menu) {
+        List<WebElement> itemsList = getListOfMenuItems();
+        return itemsList.stream()
+                .filter(e -> e.getText().equals(menu.getName()))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("Field does not found: " + menu.getName()));
+    }
+
+    public void openSection(Menu menuItems) {
+        filterList(menuItems).click();
+    }
 }
