69f9963b054f7767e468f26d55d2867ea50bf4e7
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-31 00:32:28 +0300

    Add ru.firstinspace.abramovicho.ui.ResultPageTest

---
+++ src/test/java/ru/firstinspace/abramovicho/ui/ResultPageTest.java
@@ -0,0 +1,33 @@
+package ru.firstinspace.abramovicho.ui;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.firstinspace.abramovicho.Util;
+
+public class ResultPageTest extends BaseTest {
+    String textForSearch = "рубашка";
+
+    @Test
+    @DisplayName("Test search with non-existent product name")
+    public void testSearchWithEmptyResultList() {
+        HomePage homePage = new HomePage();
+        homePage.closeSubscriptionDialog();
+        String wrongText = RandomStringUtils.randomAlphabetic(10);
+        homePage.inputTextInSearchField(wrongText);
+        ResultsPage resultsPage = new ResultsPage();
+        Assertions.assertTrue(Util.findMatchInText(resultsPage.getEmptySearchResultMessage(), wrongText));
+    }
+
+    @Test
+    @DisplayName("Test that the model from the search results contains text fragment")
+    public void testSearchResultWithExistingName() {
+        HomePage homePage = new HomePage();
+        homePage.closeSubscriptionDialog();
+        homePage.inputTextInSearchField(textForSearch);
+        ResultsPage resultsPage = new ResultsPage();
+        ModelInfoPage modelInfoPage = resultsPage.openModelDescription();
+        Assertions.assertTrue(Util.findMatchInText(modelInfoPage.getDescription(), textForSearch));
+    }
+}
