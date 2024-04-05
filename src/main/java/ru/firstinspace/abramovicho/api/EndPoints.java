329535c4effe24360ab9ae560ed03d9633907a50
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-04-06 00:06:50 +0300

    Add EndPoints.java

---
+++ src/main/java/ru/firstinspace/abramovicho/api/EndPoints.java
@@ -0,0 +1,6 @@
+package ru.firstinspace.abramovicho.api;
+
+public class EndPoints {
+    public final static String LOGIN = "/api/users/auth/login/";
+    public final static String CATALOG = "/api/catalog/list/";
+}
