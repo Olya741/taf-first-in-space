f858f77b5953a21d19423e2f1bc2a062b8dd8b1c
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-04-08 21:53:38 +0300

    Add ru.firstinspace.abramovicho.ui.UIWait

---
+++ src/main/java/ru/firstinspace/abramovicho/ui/UIWait.java
@@ -0,0 +1,20 @@
+package ru.firstinspace.abramovicho.ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class UIWait {
+        public static void waitElementIsClickable(WebDriver driver,By locator,int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static void waitElementIsVisible(WebDriver driver,By locator, int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
