08073a1e7ab93e62f8fbc8ab1bb9860e66ed3f63
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-04-10 00:01:22 +0300

    Refactor

--- src/main/java/ru/firstinspace/abramovicho/ui/UIWait.java
+++ src/main/java/ru/firstinspace/abramovicho/ui/UIWait.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class UIWait {
-        public static void waitElementIsClickable(WebDriver driver,By locator) {
+    public static void waitElementIsClickable(WebDriver driver, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
-    public static void waitElementIsVisible(WebDriver driver,By locator) {
+    public static void waitElementIsVisible(WebDriver driver, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
