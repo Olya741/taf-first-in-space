69ee99cb393ad971496107dea1dd78e00cce96a4
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-04-24 21:48:47 +0300

    Refactor BaseTest.java

--- src/test/java/ru/firstinspace/abramovicho/ui/BaseTest.java
+++ src/test/java/ru/firstinspace/abramovicho/ui/BaseTest.java
@@ -2,15 +2,13 @@ package ru.firstinspace.abramovicho.ui;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
 
 public class BaseTest {
-    WebDriver driver;
 
     @BeforeEach
     public void warmUp() {
-        driver = Driver.getDriver();
-        driver.get("https://www.first-in-space.ru/");
+        Driver.getDriver()
+                .get("https://www.first-in-space.ru/");
     }
 
     @AfterEach
